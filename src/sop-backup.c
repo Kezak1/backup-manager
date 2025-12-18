ea897b000455873475db90b25036b8b6a69d24be
Kezak1 <kenguyen701@gmail.com>
2025-12-18 18:01:55 +0100

    ello

--- src/sop-backup.c
+++ src/sop-backup.c
@@ -1,19 +1,39 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <signal.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+#include <signal.h>
 
 #define ERR(source) \
     (perror(source), fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), kill(0, SIGKILL), exit(EXIT_FAILURE))
+
+void set_handler(void (*f)(int), int sig_num) {
+    struct sigaction act;
+    memset(&act, 0, sizeof(struct sigaction));
+    act.sa_handler = f;
+
+    if(-1 == sigaction(sig_num, &act, NULL)) {
+        ERR("sigaction");
+    }
+}
+
+void sig_exit_handler(int sig_num) {
+    puts("");
+    _exit(EXIT_SUCCESS);
+}
 
 void usage() {
     ;
 }
 
-int main(int argc, const char* argv[]) {
-    printf("Hello to the backup management system by Kezak1\n");
+int main() {
+    set_handler(sig_exit_handler, SIGINT);
+    set_handler(sig_exit_handler, SIGTERM);
+
+    printf("Welcome to the backup management system!\n\n");
     while(1) {
         printf("command: ");
         char cmd[100];
